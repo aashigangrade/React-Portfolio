730a9027013f41483adc71780f5984b085208c4b
Aashi <aashigangrade06@gmail.com>
2020-09-20 22:37:04 +0530

    Updates

---
+++ precache-manifest.933cb5aea7035ddc096cc4cb772590cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091b8eb76559bf9b72e4dae6a98fe546",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "5cc1f67d7a34dbb63da4",
+    "url": "/React-Portfolio/static/css/main.9c2307c4.chunk.css"
+  },
+  {
+    "revision": "12e17fb5ec105ae79ac5",
+    "url": "/React-Portfolio/static/js/2.8793e4b4.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/React-Portfolio/static/js/2.8793e4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cc1f67d7a34dbb63da4",
+    "url": "/React-Portfolio/static/js/main.53f7f8b7.chunk.js"
+  },
+  {
+    "revision": "cb7a9505cf195efa1427",
+    "url": "/React-Portfolio/static/js/runtime-main.da5ae62e.js"
+  },
+  {
+    "revision": "c934a8a6d299bfa55c48589e25982a8c",
+    "url": "/React-Portfolio/static/media/bg-masthead.c934a8a6.jpg"
+  }
+]);
